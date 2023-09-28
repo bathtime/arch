05b1f18b45f518226b3d2d5fcf890cde9492e501
bathtime <36845712+bathtime@users.noreply.github.com>
2023-09-28 16:05:28 -0400

    Update chroot-script.sh

--- chroot-script.sh
+++ chroot-script.sh
@@ -17,7 +17,27 @@ ls -la /
 pacman -S grub efibootmgr os-prober
 
 grub-mkconfig -o /boot/grub/grub.cfg
+
 grub-install --target=i386-efi --efi-directory=/boot/ --bootloader-id=GRUB
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF
+[Service]
+Type=simple
+ExecStart=
+ExecStart=-/sbin/agetty --skip-login --nonewline --noissue --autologin user --noclear %I 38400 linux
+EOF
+
+useradd -m user -p 123456
+usermod -aG wheel user
+
+su user
+
+echo '
+if [[ ! ${DISPLAY} && ${XDG_VTNR} == 1 ]]; then
+   #startplasma-wayland
+   echo 'Autologin.'
+fi' >> ~/.profile
 
 
 echo -e '\nEntering personal bash. Type 'exit' to exit chroot\n'
@@ -26,3 +46,13 @@ echo -e '\nExiting chroot.\n'
 
 #Shouldn't be required as was run with pacstrap
 #mkinitcpio -P
+
+
+
+
+
+
+
+
+
+
