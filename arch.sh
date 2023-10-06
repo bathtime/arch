35812cd7099ea3ca7b7bb5425128d7932d4fba38
bathtime <36845712+bathtime@users.noreply.github.com>
2023-10-06 16:10:03 -0400

    Add files via upload

--- arch.sh
+++ arch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+# Run with:
+# bash <(curl -sL bit.ly/a-install)
+
+
 check_viable_disk () {
 
 if [[ "$disk" == "" ]]; then
@@ -65,12 +69,10 @@ fi
 
 choose_disk () {
 
-
 echo -e "\nDrives found:\n"
 
-lsblk --output=PATH,SIZE,MODEL,TRAN -d
-disks="$(lsblk --output=PATH -d -n)"
-
+lsblk --output=PATH,SIZE,MODEL,TRAN -d | grep -P "/dev/sd|nvme|vd"
+disks=$(lsblk -dpnoNAME|grep -P "/dev/sd|nvme|vd") 
 disks+=$(echo -e "\nquit")
 
 echo -e "\nWhich drive?\n"
@@ -96,7 +98,11 @@ check_on_root
 
 unmount_disk
 
-wipefs -a $disk 
+echo -e "\nWiping disk...\n"
+
+wipefs -af $disk 
+
+sgdisk -Zo $disk
 
 # Not sure if this is required but can't hurt
 echo "Wiping first 25mb of disk. Please be patient..."
@@ -113,42 +119,39 @@ delete_partitions
 
 parted -s $disk mklabel gpt
 parted -s --align=optimal $disk mkpart ESP fat32 1Mib 1000Mib 
-parted -s $disk set 1 esp on
+parted -s $disk set $efiPart esp on
 
 #parted -s --align=optimal $disk mkpart BOOT ext4 1001Mib 2000Mib
-parted -s --align=optimal $disk mkpart BOOT fat32 1001Mib 1004Mib 
-parted -s $disk set 2 bios_grub on
+#parted -s --align=optimal $disk mkpart BOOT fat32 1001Mib 1004Mib 
+#parted -s $disk set 2 bios_grub on
 
 parted -s --align=optimal $disk mkpart SWAP linux-swap 1005Mib 10Gib
-parted -s $disk set 3 swap on
+parted -s $disk set $swapPart swap on
 parted -s --align=optimal $disk mkpart ROOT btrfs 10Gib 100%
 
-mkfs.fat -F 32 -n EFI $disk'1'
+mkfs.fat -F 32 -n EFI $disk$efiPart
 
-mkfs.vfat -F 32 -n BIOS $disk'2'
+#mkfs.vfat -F 32 -n BIOS $disk$boisPart
 #mkfs.ext4 $disk'2'
 
-mkswap $disk'3'
-mkfs.btrfs -f -L ROOT $disk'4'
+mkswap $disk$swapPart
+mkfs.btrfs -f -L ROOT $disk$rootPart
 
 parted -s $disk print
 
 echo -e "\nMounting $mnt..."
-mount --mkdir $disk'4' $mnt
+mount --mkdir $disk$rootPart $mnt
 
 cd $mnt
 
-btrfs subvolume create @
-btrfs subvolume create @varcache
-btrfs subvolume create @varlog
-btrfs subvolume create @vartmp
-#btrfs subvolume create @snapshots
-#btrfs subvolume create @swap
+for subvol in '' "${subvols[@]}"; do
+    btrfs su cr /mnt/@"$subvol"
+done
 
 unmount_disk
-
 mount_mount
 
+mkdir -p $mnt/{etc,tmp}
 
 # Must be run here as cannot create UUIDs in chroot
 genfstab -U $mnt > $mnt/etc/fstab
@@ -165,29 +168,33 @@ check_on_root
 
 if [[ ! $(mount | grep -E "on /mnt") ]]; then
 
-mount -o compress=zstd,noatime,subvol=@ $disk'4' $mnt
+mountopts="ssd,noatime,compress-force=zstd:1,discard=async"
 
-# Not sure if this is required
 mkdir -p $mnt/{etc,tmp}
 
-mount --mkdir -o compress=zstd,noatime,nodatacow,subvol=@varcache $disk'4' $mnt/var/cache
-mount --mkdir -o compress=zstd,noatime,nodatacow,subvol=@varlog $disk'4' $mnt/var/log
-mount --mkdir -o compress=zstd,noatime,nodatacow,subvol=@vartmp $disk'4' $mnt/var/tmp
-#mount --mkdir -o compress=zstd,noatime,nodatacow,subvol=@snapshots $disk'4' $mnt/.snapshots
-#mount --mkdir -o compress=zstd,noatime,nodatacow,subvol=@swap $disk'4' $mnt/swap
+echo -e "\nMounting...\n"
+for subvol in '' "${subvols[@]}"; do
+    mount --mkdir -o "$mountopts",subvol=@"$subvol" "$disk$rootPart" $mnt/"${subvol//_//}"
+    echo "mount -o $mountopts,subvol=@$subvol $disk$rootPart /mnt/${subvol//_//}"
+done
+
+# mount efi partition
+mount --mkdir $disk$efiPart $mnt/efi
 
 # Make dirs nocow
 chattr -R +C $mnt/var/{cache,log,tmp}
 
+#chmod 750 $mnt/root
+
 # Or you'll get an error when packages try to install
 chmod 1777 $mnt/var/tmp
 
-# moutn boot partition
-#mount --mkdir $disk'2' $mnt/boot
-
-
 # mount efi partition
-mount --mkdir $disk'1' $mnt/efi
+mount --mkdir $disk$efiPart $mnt/efi
+
+# mount boot partition
+#mount --mkdir $disk$biosPart $mnt/boot
+
 
 fi
 
@@ -200,9 +207,311 @@ install_pacstrap () {
 check_on_root
 
 source /etc/profile
-pacstrap -K $mnt base linux linux-firmware btrfs-progs vim libarchive
+pacstrap -K $mnt base linux linux-firmware btrfs-progs vim vi libarchive intel-ucode
 
 }
+
+install_EFISTUB () {
+
+echo
+
+}
+
+install_GRUB () {
+
+pacstrap -K $mnt grub grub-btrfs os-prober efibootmgr inotify-tools lz4
+
+SWAP_UUID=$(blkid -s UUID -o value $disk$swapPart)
+
+arch-chroot $mnt /bin/bash -e << EOF
+
+grub-install --target=i386-pc $disk --recheck
+grub-install --target=x86_64-efi --efi-directory=/efi/ --bootloader-id=GRUB --removable
+
+cat > /etc/default/grub << EOF2
+
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR=""
+GRUB_DEFAULT=saved
+GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL_OUTPUT="console"
+GRUB_CMDLINE_LINUX="nmi_watchdog=0 loglevel=4 rd.udev.log_level=4 resume=UUID=$SWAP_UUID zswap.enabled=1 zswap.compressor=lz4 zswap.max_pool_percent=20 zswap.zpool=z3fold"
+GRUB_DISABLE_RECOVERY="true"
+GRUB_HIDDEN_TIMEOUT=2
+GRUB_RECORDFAIL_TIMEOUT=1
+GRUB_TIMEOUT=0
+ 
+# Update grub with:
+# grub-mkconfig -o /boot/grub/grub.cfg
+
+EOF2
+
+
+# Allows grub to run snapshots
+systemctl enable grub-btrfsd.service
+/etc/grub.d/41_snapshots-btrfs
+
+# Remove grub os-prober message
+sed -i 's/grub_warn/#grub_warn/g' /etc/grub.d/30_os-prober
+
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+EOF
+
+}
+
+
+
+setup_fstab () {
+
+SWAP_UUID=$(blkid -s UUID -o value $disk$swapPart)
+
+# No zram 
+#sed -i '/zram0/d' $mnt/etc/fstab
+
+# Changing compression
+sed -i 's/zstd:3/zstd:1/' $mnt/etc/fstab
+
+# genfstab will generate a swap drive. we're using a swap file instead
+sed -i '/LABEL=SWAP/d; /none.*swap.*defaults/d' $mnt/etc/fstab
+
+#echo '/dev/zram0 none swap defaults,pri=100 0 0' >> /etc/fstab
+
+[ ! "$(cat $mnt/etc/fstab | grep 'none swap defaults 0 0')" ] && echo "UUID=$SWAP_UUID none swap defaults 0 0" >> $mnt/etc/fstab
+
+# Put ~/.cache in tmpfs
+[ ! "$(cat $mnt/etc/fstab | grep 'tmpfs    /home/')" ] && echo -e "tmpfs    /home/$user/.cache    tmpfs   rw,nodev,nosuid,uid=$user,size=2G   0 0" >> $mnt/etc/fstab
+
+cat $mnt/etc/fstab
+
+}
+
+
+
+general_setup () {
+
+arch-chroot $mnt /bin/bash -e << EOF
+
+echo -e 'en_US.UTF-8 UTF-8\nen_US ISO-8859-1' > /etc/locale.gen  
+
+hwclock --systohc
+ln -sf /usr/share/zoneinfo/Canada/Eastern /etc/localtime
+
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'Arch-Linux' > /etc/hostname
+echo 'KEYMAP=us' > /etc/vconsole.conf
+
+locale-gen
+
+###  Install necessary applications with proper permissions
+mkdir -p -m 750 /etc/sudoers.d
+
+pacman --noconfirm -Sy sudo tar man
+
+pacman --noconfirm -Sy dosfstools parted arch-install-scripts snapper git base-devel less
+
+mkdir -p /etc/mkinitcpio.conf.d
+echo 'HOOKS=(base udev autodetect modconf kms keyboard sd-vconsole block filesystems resume fsck)' > /etc/mkinitcpio.conf.d/myhooks.conf
+
+mkinitcpio -p linux
+
+
+# Autologin to tty1
+
+mkdir -p /etc/systemd/system/getty@tty1.service.d
+cat > /etc/systemd/system/getty@tty1.service.d/autologin.conf << EOF2
+[Service]
+Type=simple
+ExecStart=
+ExecStart=-/sbin/agetty --skip-login --nonewline --noissue --autologin $user --noclear %I 38400 linux
+EOF2
+
+
+# Setup sudo
+mkdir -p /etc/sudoers.d
+echo "$user ALL=(ALL)  NOPASSWD: /usr/bin/btrfs-assistant-launcher" > /etc/sudoers.d/nopasswd
+echo '%wheel ALL=(ALL:ALL) ALL' > /etc/sudoers.d/wheel
+
+# Default root password is: 123456
+printf "123456\n123456\n" | passwd root
+
+useradd -m $user -G wheel
+printf "123456\n123456\n" | passwd $user
+
+# Disable login by root
+#passwd --lock root
+
+echo '[[ $- != *i* ]] && return
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias vi="vim"
+PS1="# "' > /root/.bashrc
+
+
+
+###  Setup network  ###
+
+pacman --noconfirm -Sy iw iwd dhcpcd
+
+# Helps with slow booting caused by waiting for a connection
+mkdir -p /etc/systemd/system/dhcpcd@.service.d/
+cat > /etc/systemd/system/dhcpcd@.service.d/no-wait.conf << EOF2
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dhcpcd -b -q %I
+EOF2
+
+mkdir -p /etc/iwd
+cat > /etc/iwd/main.conf << EOF2
+[General]
+EnableNetworkConfiguration=true
+EOF2
+
+# So iwd can automatically connect without any further interaction
+mkdir -p /var/lib/iwd
+cat > /var/lib/iwd/BELL364.psk << EOF2
+[Security]
+Passphrase=13FDC4A93E3C
+EOF2
+
+echo "Enabling network services..."
+systemctl enable iwd.service dhcpcd.service
+
+EOF
+
+
+
+###  Finish setting up user  ###
+
+echo '# If running bash
+if [ -n "$BASH_VERSION" ]; then
+
+    # include .bashrc if it exists
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+
+PATH="$HOME/.local/bin:$PATH"
+
+export EDITOR=/usr/bin/vi
+export QT_QPA_PLATFORM=wayland
+export QT_IM_MODULE=Maliit
+export MOZ_ENABLE_WAYLAND=1
+export XDG_RUNTIME_DIR=/run/$USER/1000
+export RUNLEVEL=3
+export QT_LOGGING_RULES="*=false"
+
+if [[ ! "${DISPLAY}" && "${XDG_VTNR}" == 1 ]]; then
+
+   if ! [ -f /run/user/$UID/runonce-setup ]; then
+      touch /run/user/$UID/runonce-setup
+      cp /arch.sh $HOME/
+      #./arch.sh --first-run
+   fi
+
+   echo "Auto-logged in."
+fi' > $mnt/home/$user/.bash_profile
+chown user:user $mnt/home/$user/.bash_profile
+
+# Remeber last cursor position in vim
+echo 'source $VIMRUNTIME/vimrc_example.vim' > /home/$user/.vimrc
+chown user:user /home/$user/.vimrc
+
+touch $mnt/home/$user/.hushlogin
+chown user:user $mnt/home/$user/.hushlogin
+
+echo '[[ $- != *i* ]] && return
+alias ls="ls --color=auto"
+alias grep="grep --color=auto"
+alias vi="vim"
+PS1="$ "' > $mnt/home/$user/.bashrc
+chown user:user $mnt/home/$user/.bashrc
+
+}
+
+setup_snapper () {
+
+arch-chroot $mnt /bin/bash -e << EOF
+
+snapper --no-dbus -c root create-config /
+snapper --no-dbus list-configs
+
+snapper --no-dbus create --read-write
+snapper --no-dbus list
+
+# Automate snapper and btrfs services  ###
+systemctl enable snapper-timeline.timer
+systemctl enable snapper-cleanup.timer
+#systemctl enable snapper-boot.timer
+ 
+#systemctl enable btrfs-balance.timer
+systemctl enable btrfs-scrub@-.timer
+#systemctl enable btrfs-trim.timer
+ 
+# Have snapper take a snapshot every 20 mins (default is every 1hr)
+mkdir -p /etc/systemd/system/snapper-timeline.timer.d/
+cat > /etc/systemd/system/snapper-timeline.timer.d/frequency.conf << EOF2
+[Timer]
+OnCalendar=
+OnCalendar=*:0/20
+EOF2
+
+echo "To edit snapper config, run: vi /etc/snapper/configs/root"
+
+# Needs to be run on inital snapshot
+/etc/grub.d/41_snapshots-btrfs
+
+systemctl enable grub-btrfsd
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+EOF
+
+}
+
+
+
+install_aur () {
+
+arch-chroot $mnt /bin/bash -e << EOF
+
+cd /home/$user
+rm -rf yay-bin
+
+sudo -u $user git clone https://aur.archlinux.org/yay-bin
+
+cd yay-bin
+sudo -u user makepkg -si
+
+sudo -u $user yay -Y --gendb
+
+EOF
+
+}
+
+
+
+install_tweaks () {
+
+arch-chroot $mnt /bin/bash -e << EOF
+
+pacman -Sy terminus-font
+
+echo 'FONT=ter-132b' >> /etc/vconsole.conf
+echo 'vm.swappiness = 10' > /etc/sysctl.d/99-swappiness.conf
+echo 'BINARIES=(setfont)' > /etc/mkinitcpio.conf.d/setfont.conf
+echo 'MODULES=(lz4)' > /etc/mkinitcpio.conf.d/lz4.conf
+
+mkinitcpio -p linux
+
+EOF
+
+
+}
+
+
 
 reset_keys () {
 
@@ -213,10 +522,10 @@ pacman-key --populate
 }
 
 
-copy_scripts () {
+copy_script () {
 
-echo -e "\nCopying scripts to $mnt\n"
-cp {arch.sh,chroot.sh,post-setup.sh} $mnt
+echo -e "\nCopying script to $mnt\n"
+cp arch.sh $mnt
 
 }
 
@@ -284,9 +593,7 @@ download_scripts () {
 
 echo -e "\nDowloading scripts from Github..."
 
-#curl -s https://raw.githubusercontent.com/bathtime/arch/main/arch.sh > arch.sh
-curl -s https://raw.githubusercontent.com/bathtime/arch/main/chroot.sh > chroot.sh
-curl -s https://raw.githubusercontent.com/bathtime/arch/main/post-setup.sh > post-setup.sh
+curl -s https://raw.githubusercontent.com/bathtime/arch/main/arch.sh > arch.sh
 
 if [[ "$?" -eq 0 ]]; then
    echo "Download successful!"
@@ -301,13 +608,18 @@ fi
 
 download_apps () {
 
-pacman -S arch-install-scripts 
+pacman -S arch-install-scripts gptfdisk terminus-font 
 
 }
 
 
 
 mnt=/mnt
+efiPart=1
+biosPart=0
+swapPart=2
+rootPart=3
+subvols=(var_cache var_log var_tmp)
 user=user
 
 # Make font big and readable
@@ -348,7 +660,13 @@ choices=(
 "Choose disk"
 "Partition disk"
 "Install pacstrap"
-"Copy scripts"
+"Install boot manager"
+"Setup fstab"
+"General setup"
+"Install aur"
+"Install tweaks"
+"Setup snapper"
+"Copy script"
 "Chroot install"
 "Chroot"
 "Mount $mnt"
@@ -370,13 +688,32 @@ do
         "Choose disk")		choose_disk ;;
         "Partition disk")	create_partitions ;;
         "Install pacstrap")	install_pacstrap ;;
+	"Install boot manager") echo -e "\nWhich boot manager would you like to install?\n"
+		
+				choiceBoot=(grub efiSTUB rEFInd quit) 
+
+				select choiceBoot in "${choiceBoot[@]}"
+				do
+					case $choiceBoot in
+						"grub")		install_GRUB ;;
+						"efiSTUB")	install_EFISTUB ;;
+						"rEFInd")	install_REFIND ;;
+						"quit")		exit ;;
+						'')		echo -e "\nInvalid option!\n" ;;
+					esac						
+				done ;;
+        "Setup fstab")		setup_fstab ;;
+	"General setup")	general_setup ;;
+        "Install aur")		install_aur ;;
+        "Install tweaks")	install_tweaks ;;
+	"Setup snapper")	setup_snapper ;;
         "Chroot")		do_chroot ;;
         "Chroot install")	chroot_install ;;
         "Mount $mnt")		mount_mount  ;;
         "Unmount $mnt")		unmount_disk  ;;
         "Print partitions")	print_partitions ;;
         "Delete partitions")	delete_partitions ;;
-        "Copy scripts")		copy_scripts ;;
+        "Copy script")		copy_scripts ;;
         "Connect wireless")	connect_wireless ;;
         "Download scripts")	download_scripts ;;
         "Download apps")	download_apps    ;;
