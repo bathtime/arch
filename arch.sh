929f1df9a029efe917fbca8f0b4ab9cd544bf1fb
bathtime <36845712+bathtime@users.noreply.github.com>
2023-10-07 01:41:32 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -358,7 +358,7 @@ if [ "$encrypt" -eq 1 ]; then
 
 else
 
-   echo 'HOOKS=(base udev autodetect modconf kms keyboard sd-vconsole block filesystems resume fsck)' > /etc/mkinitcpio.conf.d/myhooks.conf
+   echo 'HOOKS=(systemd autodetect modconf keyboard sd-vconsole block filesystems resume fsck)' > /etc/mkinitcpio.conf.d/myhooks.conf
 
 fi
 
