c880e5e39c3b0277167bf04c9970342d30fc0769
bathtime <36845712+bathtime@users.noreply.github.com>
2023-10-08 11:14:18 -0400

    Add files via upload

--- config.sh
+++ config.sh
@@ -4,9 +4,11 @@ cd /home/user
 
 
 FILES="
-
+.local/share/kxmlgui5/konsole/konsoleui.rc
 .config/kded5rc
+.config/konsolerc
 .local/share/konsole/*.profile
+.config/plasmashellrc
 .config/kwinrulesrc
 .config/ksmserverrc
 .config/plasma-org.kde.plasma.desktop-appletsrc
@@ -19,12 +21,12 @@ FILES="
 for FILE in $FILES
 do
     ls -la "$FILE"
-    #rm "$FILE"
+    rm -rf "$FILE"
 done
 
 
 
-tar cvf setup.tar $FILES
+#tar cvf setup.tar $FILES
 
 exit
 
