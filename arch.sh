3c016109b6a439d8c6f2627634e9f8436ab9d4d2
bathtime <36845712+bathtime@users.noreply.github.com>
2023-10-17 16:54:48 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -652,7 +652,7 @@ create_archive() {
 
    rm -rf /real_root/@/root.squashfs
 
-   mksquashfs . /real_root/@/root.squashfs -noappend -no-recovery -mem-percent 50 -e *rootfs.tar.gz -e *root.squashfs -e ./boot/ -e ./efi/ -e ./dev/ -e ./proc/ -e ./sys -e ./tmp -e ./run -e ./mnt -e ./.snapshots/ -e ./var/tmp/ -e ./var/cache/ -e ./var/log/ -e ./etc/pacman.d/gnupg/
+   mksquashfs . /real_root/@/root.squashfs -noappend -no-recovery -mem-percent 50 -e *rootfs.tar.gz -e *root.squashfs -e ./boot/ -e ./efi/ -e ./dev/ -e ./proc/ -e ./sys -e ./tmp -e ./run -e ./mnt -e ./.snapshots/ -e ./var/tmp/ -e ./var/cache/ -e ./var/log/ -e ./etc/pacman.d/gnupg/  -e ./var/lib/systemd/random-seed
 
    ls -la /real_root/@/root.squashfs
 
@@ -751,14 +751,14 @@ run_latehook() {
 
          fi
 
-         touch /new_root/LIVE
-         umount /real_root/
+         #touch /new_root/LIVE
+         umount -l /real_root/
 
-         LIVE_mount() {
-            echo "Live system on RAM."
-         }
+         #LIVE_mount() {
+         #   echo "Live system on RAM."
+         #}
          
-         mount_handler=LIVE_mount
+         #mount_handler=LIVE_mount
 
       elif [[ "$key" = "d" ]]; then
 
